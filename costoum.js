e3ff2d6b03d8c694b32c8238a43ab496fe71592c
rokon <ismail123ismail200@gmail.com>
2024-02-19 21:23:59 +0600

    first comm

--- costoum.js
+++ costoum.js
@@ -12,13 +12,14 @@ let btn = document.getElementsByClassName("bnt");
 
 let dowen = 24;
 let count = 0;
-for(const button of btn){
-  button.addEventListener("click",function(){ 
+for(let button of btn){
+  button.addEventListener("click",function(event){ 
 
     let chack = document.getElementById("spn").innerText;
     if(chack>3){
-      return alert("invalid")
+      return alert("sorry")
     }
+
     count += 1;
     dowen -= 1;
 
@@ -26,8 +27,33 @@ for(const button of btn){
     button.style.backgroundColor="green";
     button.classList.add("disabled:");
     
-      let ponero = document.getElementById("porero1").innerText;
-      let finelset = parseInt(ponero);
+    let ponero = document.getElementById("porero1").innerText;
+    let finelset = parseInt(ponero);
+
+
+
+    let sit = event.target.innerText;
+    
+    let tk = event.target.parentNode.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[3].childNodes[1].innerText;
+
+    let Ekonommik = event.target.parentNode.parentNode.parentNode.parentNode.childNodes[5].childNodes[3].childNodes[7].childNodes[1].innerText;
+
+    let allseti = document.getElementById("selected-place-containr");
+    
+    //console.log(allseti);
+
+    const li = document.createElement("li");
+    const p = document.createElement("p");
+    p.innerText = sit;
+    const p2 = document.createElement("p");
+    p2.innerText = Ekonommik;
+    const p3 = document.createElement("p");
+    p3.innerText = tk;
+
+    li.appendChild(p);
+    li.appendChild(p2);
+    li.appendChild(p3);
+    allseti.appendChild(li);
 
 // function call
     set("spn",count);
@@ -43,7 +69,6 @@ for(const button of btn){
 function set(id,pash){
     let sum = document.getElementById(id);
     sum.innerText = pash;
-
 }
 
 
@@ -52,4 +77,13 @@ function numnu(id,vale){
   let convart = parseInt(num);
   document.getElementById(id).innerText = convart + parseInt(vale);
       
+}
+
+function sussessly(){
+  const fot = document.getElementById("main");
+  fot.classList.add("hidden");
+
+    const fot1 = document.getElementById("sussess");
+    fot1.classList.remove("hidden");
+    // console.log("ergthysr")
 }
